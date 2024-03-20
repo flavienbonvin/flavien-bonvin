f0e35490ec6d1bf9f770385e23e66d1c93d9b98f
Flavien Bonvin <flavien.bonvin@pm.me>
2024-03-20 22:22:51 +0100

    chore: add tailwind and prettier

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+const config = {
+    printWidth: 100,
+    tabWidth: 4,
+    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+};
+
+export default config;
