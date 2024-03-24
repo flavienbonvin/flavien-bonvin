bf506c670af6b21bc2af22dbea6fa3efb1ae5dcf
Flavien Bonvin <flavien.bonvin@pm.me>
2024-03-24 20:04:40 +0100

    feat: add custom fonts to project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -6,6 +8,10 @@ export default {
             center: true,
         },
         extend: {
+            fontFamily: {
+                sans: ["Open Sans Variable", ...defaultTheme.fontFamily.sans],
+                serif: ["Tinos", ...defaultTheme.fontFamily.serif],
+            },
             colors: {
                 background: "hsl(208.4, 25.3%, 14.7%)",
                 "font-color": "hsl(222, 11%, 91%)",
