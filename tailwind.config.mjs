5bee249f6e9b897fcd396f86f0927f3bd5386c98
Flavien Bonvin <flavien.bonvin@pm.me>
2024-03-24 11:06:54 +0100

    feat: create basic page layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                background: "hsl(208.4, 25.3%, 14.7%)",
+                "font-color": "hsl(222, 11%, 91%)",
+                "font-color-foreground": "hsl(222, 20%, 95%)"
+            }
+        },
     },
-    plugins: [],
+    plugins: [require('@tailwindcss/typography')],
 };
