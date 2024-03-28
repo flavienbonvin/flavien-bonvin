b438d6878bac495672f01e840cb874f10e7f9c75
Flavien Bonvin <flavien.bonvin@pm.me>
2024-03-28 14:38:39 +0100

    fix: add vercel dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,25 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.flavienbonvin.com/",
-    integrations: [
-        tailwind(),
-        sitemap(),
-        mdx({
-            syntaxHighlight: "shiki",
-            shikiConfig: {
-                theme: "dracula",
-            },
-        }),
-        icon(),
-    ],
-    markdown: {
-        syntaxHighlight: "shiki",
-        shikiConfig: {
-            theme: "dracula",
-        },
-    },
+  site: "https://www.flavienbonvin.com/",
+  integrations: [tailwind(), sitemap(), mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "dracula"
+    }
+  }), icon()],
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "dracula"
+    }
+  },
+  output: "server",
+  adapter: vercel()
 });
