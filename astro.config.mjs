75417c709bb2a1d7a109d4d71b32746b2db19dcd
Flavien Bonvin <flavien.bonvin@pm.me>
2024-03-28 14:51:25 +0100

    fix: remove vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,27 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.flavienbonvin.com/",
-  integrations: [tailwind(), sitemap(), mdx({
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "dracula"
-    }
-  }), icon()],
-  markdown: {
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "dracula"
-    }
-  },
-  output: "server",
-  adapter: vercel()
+    site: "https://www.flavienbonvin.com/",
+    integrations: [
+        tailwind(),
+        sitemap(),
+        mdx({
+            syntaxHighlight: "shiki",
+            shikiConfig: {
+                theme: "dracula",
+            },
+        }),
+        icon(),
+    ],
+    markdown: {
+        syntaxHighlight: "shiki",
+        shikiConfig: {
+            theme: "dracula",
+        },
+    },
 });
