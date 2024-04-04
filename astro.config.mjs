7f26498c33f6cbe0146ea58a7667bc69a05d78d0
Flavien Bonvin <flavien.bonvin@pm.me>
2024-04-04 14:03:13 +0200

    chore: update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.flavienbonvin.com/",
+    site: "https://flavien-bonvin.vercel.app/",
     integrations: [
         tailwind(),
         sitemap(),
