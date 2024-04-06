ef30a0dd30451e671ea6459cb5d11b25e0c0ed34
Flavien Bonvin <flavien.bonvin@pm.me>
2024-04-06 12:28:02 +0200

    feat: rework responsive

--- src/const.ts
+++ src/const.ts
@@ -12,3 +12,5 @@ export const CONFIG = {
     description:
         "Flavien Bonvin blog where you I write about software development, and any other topic that spark my interest.",
 } as const;
+
+export const articleCardWidth = "sm:w-5/6 md:w-4/5";
