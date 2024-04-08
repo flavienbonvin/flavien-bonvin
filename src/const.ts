0b30c57ade4845991b8147041eebcb502c52634e
Flavien Bonvin <flavien.bonvin@pm.me>
2024-04-08 22:07:14 +0200

    feat: update page title and extract methods

--- src/const.ts
+++ src/const.ts
@@ -6,11 +6,21 @@ export const PATHS = {
     beyond: "/articles/beyond",
 } as const;
 
+export const URLS = {
+    twitter: "https://twitter.com/FlavienBonvin",
+    github: "https://github.com/flavienbonvin",
+    linkedin: "https://www.linkedin.com/in/flavien-b/",
+    mailto: "mailto:hello@flavienbonvin.com",
+} as const;
+
 export const CONFIG = {
     author: "Flavien Bonvin",
     twitter: "FlavienBonvin",
+    homeDescription: "Insights on coding and beyond. Explore my world.",
+    devDescription: "Mastering web development. Join my journey.",
+    beyondDescription: "Life outside of coding. Discover my interests.",
     description:
-        "Flavien Bonvin blog where you I write about software development, and any other topic that spark my interest.",
+        "Explore the world of web development and beyond with Flavien Bonvin. From coding to coffee machines, productivity tools to life adventures, discover Flavien's insights and passions.",
 } as const;
 
 export const articleCardWidth = "sm:w-5/6 md:w-4/5";
