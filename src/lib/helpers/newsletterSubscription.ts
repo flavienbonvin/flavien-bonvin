e8143564d2e4dfe73e4dc716bcb0a8b9d2e2625e
Flavien Bonvin <flavien.bonvin@pm.me>
2024-08-08 21:50:52 +0200

    feat: update newsletter template

--- src/lib/helpers/newsletterSubscription.ts
+++ src/lib/helpers/newsletterSubscription.ts
@@ -26,37 +26,139 @@ export const sendNewsletterSubscriptionEmail = async (token: string, email: stri
 
 const getNewsletterTemplate = (token: string) => {
     return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-  <html dir="ltr" lang="en">
-  
+<html dir="ltr" lang="en">
     <head>
-      <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
-      <meta name="x-apple-disable-message-reformatting" />
+        <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
+        <meta name="x-apple-disable-message-reformatting" />
     </head>
-    <div style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">The sales intelligence platform that helps you uncover qualified leads.<div> ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿</div>
-    </div>
-  
-    <body style="background-color:#ffffff;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,Roboto,Oxygen-Sans,Ubuntu,Cantarell,&quot;Helvetica Neue&quot;,sans-serif">
-      <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="max-width:37.5em;margin:0 auto;padding:20px 0 48px">
-        <tbody>
-          <tr style="width:100%">
-            <td><img alt="Koala" height="50" src="https://react-email-demo-9fn3mchcm-resend.vercel.app/static/koala-logo.png" style="display:block;outline:none;border:none;text-decoration:none;margin:0 auto" width="170" />
-              <p style="font-size:16px;line-height:26px;margin:16px 0">Hi <!-- -->Alan<!-- -->,</p>
-              <p style="font-size:16px;line-height:26px;margin:16px 0">Welcome to Koala, the sales intelligence platform that helps you uncover qualified leads and close deals faster.</p>
-              <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="text-align:center">
-                <tbody>
-                  <tr>
-                    <td><a href="https://getkoala.com" style="line-height:100%;text-decoration:none;display:block;max-width:100%;background-color:#5F51E8;border-radius:3px;color:#fff;font-size:16px;text-align:center;padding:12px 12px 12px 12px" target="_blank"><span><!--[if mso]><i style="mso-font-width:300%;mso-text-raise:18" hidden>&#8202;&#8202;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:9px">Get started</span><span><!--[if mso]><i style="mso-font-width:300%" hidden>&#8202;&#8202;&#8203;</i><![endif]--></span></a></td>
-                  </tr>
-                  <tr>${token}</tr>
-                </tbody>
-              </table>
-              <p style="font-size:16px;line-height:26px;margin:16px 0">Best,<br />The Koala team</p>
-              <hr style="width:100%;border:none;border-top:1px solid #eaeaea;border-color:#cccccc;margin:20px 0" />
-              <p style="font-size:12px;line-height:24px;margin:16px 0;color:#8898aa">470 Noor Ave STE B #1148, South San Francisco, CA 94080</p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <style>
+        body {
+            background-color: #ffffff;
+            color: #303030;
+            font-family:
+                -apple-system,
+                BlinkMacSystemFont,
+                Segoe UI,
+                Roboto,
+                Oxygen,
+                Ubuntu,
+                Cantarell,
+                Fira Sans,
+                Droid Sans,
+                Helvetica Neue,
+                sans-serif;
+        }
+        a {
+            font-size: 14px;
+            color: #008574;
+        }
+        p {
+            font-size: 14px;
+        }
+    </style>
+    <body>
+        <table
+            align="center"
+            width="100%"
+            border="0"
+            cellpadding="0"
+            cellspacing="0"
+            role="presentation"
+            style="max-width: 37.5em; padding-left: 12px; padding-right: 12px; margin: 0 auto"
+        >
+            <tbody>
+                <tr style="width: 100%">
+                    <td>
+                        <h1 style="font-size: 24px; font-weight: bold; margin: 40px 0; padding: 0">
+                            Welcome and thanks you for subscribing!
+                        </h1>
+                        <p>
+                            You're just one step away from getting my lovely articles delivered
+                            right to your inbox!
+                        </p>
+                        <p>
+                            I want to confirm that the email address you provided is yours because
+                            neither Google, Proton, nor I like to send emails to the wrong
+                            addresses!
+                        </p>
+                        <a
+                            href="https://flavienbonvin.com/newsletter/validate?token=${token}"
+                            style="
+                                text-decoration: underline;
+                                display: block;
+                                margin: 24px 0;
+                                font-size: 16px;
+                                text-align: center;
+                            "
+                            target="_blank"
+                            >Click here to confirm your email address</a
+                        >
+                        <p style="line-height: 24px; margin: 24px 0; margin-bottom: 14px">
+                            Alternatively, you can copy and paste this URL to your browser:
+                        </p>
+                        <code
+                            style="
+                                display: inline-block;
+                                padding: 16px 4.5%;
+                                width: 90.5%;
+                                font-size: 12px;
+                                background-color: #f4f4f4;
+                                border-radius: 5px;
+                                border: 1px solid #eee;
+                                margin-bottom: 24px;
+                            "
+                            >https://flavienbonvin.com/newsletter/validate?token=${token}</code
+                        >
+                        <p>Have a lovely day!</p>
+                        <p>Flavien 🦆</p>
+                        <div style="border-bottom: 1px solid #ababab; margin: 24px 0"></div>
+                        <p
+                            style="
+                                font-size: 14px;
+                                line-height: 24px;
+                                margin: 24px 0;
+                                color: #ababab;
+                                margin-top: 14px;
+                                margin-bottom: 32px;
+                            "
+                        >
+                            If you didn't subscribed to my newsletter, you can safely ignore this.
+                            But you might miss on some exciting articles!
+                        </p>
+                        <img
+                            alt="Flavien Bonvin logo"
+                            height="32"
+                            width="32"
+                            src="https://flavienbonvin.com/android-chrome-192x192.png"
+                            style="
+                                display: block;
+                                outline: none;
+                                border: none;
+                                text-decoration: none;
+                            "
+                        />
+                        <p
+                            style="
+                                line-height: 22px;
+                                margin: 16px 0;
+                                color: #898989;
+                                margin-top: 12px;
+                                margin-bottom: 24px;
+                            "
+                        >
+                            <a
+                                href="https://flavienbonvin.com/"
+                                style="color: #898989; text-decoration: underline"
+                                target="_blank"
+                                >flavienbonvin.com</a
+                            >
+                            🦆
+                        </p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </body>
-  </html>`;
+</html>
+`;
 };
