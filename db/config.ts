ccff83778a34cc886749fd4ce7390eb33d82ef06
Flavien Bonvin <flavien.bonvin@pm.me>
2025-03-16 19:21:42 +0100

    Improve data insertion in db

--- db/config.ts
+++ db/config.ts
@@ -3,7 +3,7 @@ import { column, defineDb, defineTable } from "astro:db";
 const PageView = defineTable({
     columns: {
         id: column.number({ primaryKey: true }),
-        pathname: column.text(),
+        pathname: column.text({ unique: true }),
         count: column.number({ default: 1 }),
     },
 });
--- src/lib/data/pageview.ts
+++ src/lib/data/pageview.ts
@@ -1,18 +1,11 @@
-import { db, eq, PageView } from "astro:db";
+import { db, eq, PageView, sql } from "astro:db";
 
-export const getPageView = async (pathname: string) => {
-    return await db
-        .select({ count: PageView.count })
-        .from(PageView)
-        .where(eq(PageView.pathname, pathname));
-};
-
-export const createPageView = async (pathname: string) => {
-    await db.insert(PageView).values({
-        pathname,
-    });
-};
-
-export const updatePageViewCount = async (pathname: string, count: number) => {
-    await db.update(PageView).set({ count }).where(eq(PageView.pathname, pathname));
+export const upsertPageView = async (pathname: string) => {
+    await db
+        .insert(PageView)
+        .values({ pathname, count: 1 })
+        .onConflictDoUpdate({
+            target: PageView.pathname,
+            set: { count: sql`${PageView.count} + 1` },
+        });
 };
