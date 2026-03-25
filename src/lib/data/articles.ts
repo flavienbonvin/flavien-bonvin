8bac16f082cddf5b11a507f8ded098607ec8ad53
Flavien Bonvin <flavien.bonvin@pm.me>
2026-03-25 22:15:48 +0100

    Refresh the design of the list of article and header

--- src/lib/data/articles.ts
+++ src/lib/data/articles.ts
@@ -1,10 +1,12 @@
 import { getCollection, getEntry, type CollectionEntry } from "astro:content";
 import { BlogType } from "content.config";
 
-const sortArticles = (articles: CollectionEntry<"blog">[]) => {
-    return articles.sort((a, b) => {
-        return b.data.publicationDate.getTime() - a.data.publicationDate.getTime();
-    });
+const sortArticles = (articles: CollectionEntry<"blog">[], limit?: number) => {
+    return articles
+        .sort((a, b) => {
+            return b.data.publicationDate.getTime() - a.data.publicationDate.getTime();
+        })
+        .slice(0, limit ? limit : -1);
 };
 
 export const getAllPublishedArticles = async () => {
@@ -15,19 +17,19 @@ export const getAllPublishedArticles = async () => {
     });
 };
 
-export const getDevArticles = async () => {
+export const getDevArticles = async (limit?: number) => {
     return await getCollection("blog", ({ data }) => {
         return data.category === BlogType.dev;
     }).then((articles) => {
-        return sortArticles(articles);
+        return sortArticles(articles, limit);
     });
 };
 
-export const getBeyondArticle = async () => {
+export const getBeyondArticle = async (limit?: number) => {
     return getCollection("blog", ({ data }) => {
         return data.category === BlogType.beyond;
     }).then((articles) => {
-        return sortArticles(articles);
+        return sortArticles(articles, limit);
     });
 };
 
