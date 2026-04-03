24b4aa8cdc7bafe2be46c17fd83979a1af553df6
Flavien Bonvin <flavien.bonvin@pm.me>
2026-04-03 16:12:27 +0200

    chore: configure Prettier, Tailwind, and path aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+const config = {
+    tabWidth: 4,
+    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+};
+
+export default config;
