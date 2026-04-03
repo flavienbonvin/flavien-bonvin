c998a9c3c06eec800448f8646ddbc614bf1da2a7
Flavien Bonvin <flavien.bonvin@pm.me>
2026-04-03 15:08:04 +0200

    build: add base Wrangler config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  adapter: cloudflare(),
+});
