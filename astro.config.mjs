df8ce312172de93d56fd29bc9137e8ca72b738f9
Flavien Bonvin <flavien.bonvin@pm.me>
2026-04-03 16:59:17 +0200

    feat: add custom fonts and theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
@@ -7,10 +7,116 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  adapter: cloudflare(),
+    output: "static",
+    adapter: cloudflare(),
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: "Swiss works",
+            cssVariable: "--font-swiss-works",
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/SuisseWorks-Bold-WebS.woff2"],
+                        weight: "700",
+                    },
+                    {
+                        src: [
+                            "./src/assets/fonts/SuisseWorks-BoldItalic-WebS.woff2",
+                        ],
+                        style: "italic",
+                        weight: "700",
+                    },
+                    {
+                        src: [
+                            "./src/assets/fonts/SuisseWorks-Medium-WebS.woff2",
+                        ],
+                        weight: "500",
+                    },
+                    {
+                        src: [
+                            "./src/assets/fonts/SuisseWorks-MediumItalic-WebS.woff2",
+                        ],
+                        style: "italic",
+                        weight: "500",
+                    },
+                    {
+                        src: [
+                            "./src/assets/fonts/SuisseWorks-Regular-WebS.woff2",
+                        ],
+                        weight: "400",
+                    },
+                    {
+                        src: [
+                            "./src/assets/fonts/SuisseWorks-RegularItalic-WebS.woff2",
+                        ],
+                        style: "italic",
+                        weight: "400",
+                    },
+                ],
+            },
+            fallbacks: [
+                "Georgia",
+                "Cambria",
+                "Times New Roman",
+                "Times",
+                "serif",
+            ],
+        },
+        {
+            provider: fontProviders.local(),
+            name: "Berkeley mono",
+            cssVariable: "--font-berkeley-mono",
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/BerkeleyMono-Regular.woff2"],
+                        weight: "400",
+                    },
+                    {
+                        src: [
+                            "./src/assets/fonts/BerkeleyMono-Regular-Oblique.woff2",
+                        ],
+                        style: "italic",
+                        weight: "400",
+                    },
+                    {
+                        src: ["./src/assets/fonts/BerkeleyMono-Medium.woff2"],
+                        weight: "500",
+                    },
+                    {
+                        src: [
+                            "./src/assets/fonts/BerkeleyMono-Medium-Oblique.woff2",
+                        ],
+                        weight: "500",
+                        style: "italic",
+                    },
+                    {
+                        src: ["./src/assets/fonts/BerkeleyMono-Bold.woff2"],
+                        weight: "700",
+                    },
+                    {
+                        src: [
+                            "./src/assets/fonts/BerkeleyMono-Bold-Oblique.woff2",
+                        ],
+                        weight: "700",
+                        style: "italic",
+                    },
+                ],
+            },
+            fallbacks: [
+                "Menlo",
+                "Monaco",
+                "Consolas",
+                "Liberation Mono",
+                "Courier New",
+                "monospace",
+            ],
+        },
+    ],
 });
